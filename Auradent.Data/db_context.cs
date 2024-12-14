42615b83dbd5c279473f325404aa7c0a494ed7ec
hmahmoud2211 <161122276+hmahmoud2211@users.noreply.github.com>
2024-12-14 19:06:05 +0200

    updated database

---
+++ Auradent.Data/db_context.cs
@@ -0,0 +1,19 @@
+﻿using Auradent.core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Auradent.Data
+{
+    public class db_context : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=auradent.db");
+        }
+    }
+}

f1f9e24fc06f0b706ffb66f53e31a849608b2ecf
hmahmoud2211 <161122276+hmahmoud2211@users.noreply.github.com>
2024-12-20 19:17:45 +0200

    database creation is finished

--- Auradent.Data/IdataHelper.cs
+++ Auradent.Data/IdataHelper.cs
@@ -8,22 +8,21 @@ namespace Auradent.Data
 {
     public interface IdataHelper<Table>
     {
-        // read all data from the table
-        Task<List<Table>> GetAllData();
-        // read data from the table by id
-        Task<List<Table>> GetDataById(int id);
-        // insert data into the table
-        Task InsertData(Table data);
-        // update data in the table
-        Task UpdateData(Table data);
-        // delete data from the table
-        Task DeleteData(int id);
-        // search data in the table
-        Task<List<Table>> SearchData(string search);
-        // check if data exists in the table
-        Task<bool> DataExists(int id);
-        // find the data in the table
-        Task<Table> FindData(int id);
+        public List<Table> GetAllData();
+
+        public List<Table> Search(string searchItem);
+
+        public Table Find(Table item);
+
+        public bool CheckIfIdExists(int id);
+
+
+
+        //Writing
+        public List<Table> Add_list (Table table);
+        public int Add(Table table);
+        public int Update(Table table);
+        public int Delete(Table item);
     }
 
 }
---
+++ Auradent.Data/MedicineEF.cs
@@ -0,0 +1,102 @@
+﻿using Auradent.core;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auradent.Data
+{
+    public class MedicineEF : IdataHelper<Medicine>
+    {
+        private db_context db;
+        private Medicine table;
+
+        public MedicineEF()
+        {
+            db = new db_context();
+            table = new Medicine();
+        }
+        public int Add(Medicine table)
+        {
+            try
+            {
+                db.Medicine.Add(table);
+                db.SaveChangesAsync();
+                return 1;
+            }
+            catch { return 0; }
+        }
+
+        public List<Medicine> Add_list(Medicine table)
+        {
+            try
+            {
+                db.Medicine.Add(table);
+                db.SaveChanges();
+                return db.Medicine.ToList();
+            }
+            catch
+            {
+                return new List<Medicine>();
+            }
+        }
+
+        public bool CheckIfIdExists(int id)
+        {
+            return db.Medicine.Any(p => p.ID == id);
+        }
+
+        public int Delete(Medicine item)
+        {
+            try
+            {
+                table = Find(item);
+                db.Medicine.Remove(table);
+                db.SaveChanges();
+                return 1;
+            }
+            catch { return 0; }
+        }
+
+        public Medicine Find(Medicine item)
+        {
+            try
+            {
+                return db.Medicine.Where(x => x.ID == item.ID).FirstOrDefault() ?? new Medicine();
+            }
+            catch { return new Medicine(); }
+        }
+
+        public List<Medicine> GetAllData()
+        {
+            try
+            {
+                return db.Medicine.ToListAsync().Result;
+            }
+            catch { return new List<Medicine>(); }
+        }
+
+        public List<Medicine> Search(string searchItem)
+        {
+            try
+            {
+                return db.Medicine.Where(x => x.ID.ToString().Contains(searchItem) || (x.MedicineName != null && x.MedicineName.Contains(searchItem))).ToList();
+            }
+            catch { return new List<Medicine>(); }
+        }
+
+        public int Update(Medicine table)
+        {
+            try
+            {
+                db = new db_context();
+                db.Medicine.Update(table);
+                db.SaveChanges();
+                return 1;
+            }
+            catch { return 0; }
+        }
+    }
+}
